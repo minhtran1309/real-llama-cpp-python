c53a47ce3f324f6edcf62bf6918c13a07a65b1c1
DrM <tranngocminh139@gmail.com>
2025-01-03 14:29:40 +1100

    finished the test

--- test/test_object.py
+++ test/test_object.py
@@ -1,12 +1,12 @@
 import unittest
-from real_llama_cpp import CustomLlamaCpp
+from real_llamacpp import CustomLlamaCpp
 from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
 from langchain_core.output_parsers import StrOutputParser
 
 if __name__ == "__main__":
     model_path = "/home/minhtran/Projects/clinical_trial_llm_translation/models/Mistral-Nemo-Instruct-2407.Q5_K_S.gguf"
     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-    llm = CustomLLM(model_path=model_path, temperature=0.,
+    llm = CustomLlamaCpp(model_path=model_path, temperature=0.,
         max_tokens=900,n_ctx=50000,
         n_gpu_layers=40,
         top_p=1,
