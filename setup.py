e60cf592551faae6707ea4fcee0e6e40fbee32a9
DrM <tranngocminh139@gmail.com>
2025-01-15 17:55:10 +1100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="real-llama-cpp-python",
-    version="0.1.4",
+    version="0.1.5",
     description="A simple custom LLM wrapper for llama.cpp with LangChain compatibility.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
